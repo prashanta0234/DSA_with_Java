904476d7d0e472c4d0fce3112edaf0cb678f9add
prashanta0234 <prashanta0234@gmail.com>
2023-03-24 09:36:14 +0600

    add factorial

---
+++ src/dsa_with_java/Factorial.java
@@ -0,0 +1,16 @@
+package dsa_with_java;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int number=3;
+		int result=1;
+		for(int i=1; i<= number; i++) {
+			result*=i;
+		}
+		System.out.println(result);
+
+	}
+
+}
